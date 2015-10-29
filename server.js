cb5de74b1eacaba064ced464b0d1a697a4532a21
Nekorsis <Horick.94@mail.ru>
2015-10-29 18:49:41 +0600

    First version
    Basic chat on sockets

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
-var app = require('express')();
+var app = require ('express')();
 var express = require('express');
 var http = require('http').Server(app);
-
-app.get('/', function (req, res){
-  res.render('main');
-});
-
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 app.set('view engine', 'jade');
 app.use(express.static('static'));
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function (){
 });
+
+app.get('/', function (req, res){
+	res.render('main');
+});
+
+io.on('connection', function (socket){
+	socket.on('send_msg', function (msg){
+		io.sockets.emit('message', {message: msg,
+			username: socket.username});
+	});
+
+	socket.on('add user', function (username){
+			socket.username = username;
+			io.sockets.emit('user joined', socket.username);
+	});
+
+	socket.on('disconnect', function(){
+		io.sockets.emit('user left', socket.username);
+	});
+});
+
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,36 @@
-jQuery(document).ready(function(){
-    jQuery("body").css(
-        "background-color",
-        "green"
-    );
+var socket = io();
+$(function(){
+	$('#messageform').hide();
 });
+$(document).ready(function (){
+	$('#messageform').submit(function(){
+		var tempstr = $('#m').val();
+    	socket.emit('send_msg', tempstr);
+    $('#m').val('');
+    return false;
+  });
+});
+
+socket.on('message', function (data){
+	$('#messages').append($('<li>').text(data.username + ': ' + data.message));
+});
+
+socket.on('user joined', function (username){
+	$('#messages').append($('<li>').text(username + ' joined chat'));
+});
+
+socket.on('user left', function (username){
+	$('#messages').append($('<li>').text(username + ' left chat'));
+});
+
+
+$(function(){
+	$('#loginform').submit(function(){
+		var name = $('#logininput').val();
+		socket.emit('add user', name);
+		$('#loginform').hide();
+		$('#messageform').show();
+		return false;
+	});
+});
+	
